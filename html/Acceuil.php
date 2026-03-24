c99d9d4475c29d8ec2bfd8cd31f2879017c132ff
keplane <123tyreza@gmail.com>
2026-03-24 08:59:49 +0100

    [update] Footer some modification

--- html/Acceuil.php
+++ html/Acceuil.php
@@ -40,9 +40,12 @@
 
     <section class ="efrei-section">
         <div class="efrei"> <!-- qui a fait ca ? ( c'est normal que c'était cassée ? ") -->
-    <footer>
+    </section> 
 
 
+    <footer id="footer">
+    <p>© 2026 Efrei – Tous droits réservés</p><br>
+    <p><a><img src="../img/Footer_Efrei_Pick.png" href="./Acceuil.php" id="Footer_img_Logo"></a></p><br>
     </footer>
 
 </body>
