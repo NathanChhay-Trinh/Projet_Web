3b2582684d515054de8776c1d1d35b3c59754559
Nathan Chhay-Trinh <NathanC.T2788@gmail.com>
2026-03-24 12:20:07 +0100

    Formations.php + Formation.css
    Ajout des icons de bases des formations

--- html/Formations.php
+++ html/Formations.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/Interface_de_base.css">
+    <link rel="stylesheet" href="../css/Formations.css">
     <title>Formations</title>
 
 </head>
@@ -12,6 +13,18 @@
 <body>
 
     <?php include("menu.php"); ?>
+
+    <div id="formation_container">
+        <div class="formation">
+            <img src="../img/Grande_ecole.jpg" class="Grande_ecole" alt="Logo Grande Ecole">
+            Prog. Grande École
+        </div>
+        <div class="formation">
+            <img src="../img/Technologie&Numerique.jpg" class="Technologie&Numerique" alt="Logo Technologie & Numerique">
+            Prog. Technologie & Numérique
+        </div>
+        </div>
+
     <?php include("Footer.php"); ?>
 
 </body>
