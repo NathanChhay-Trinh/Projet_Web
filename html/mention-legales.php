bab9b05775ce7937c4251fa51288feaf2e4d69a5
keplane <123tyreza@gmail.com>
2026-03-28 21:36:32 +0100

    [update] css mention légales terminer ( je pense que on est good sur ca )
    dit moi si vous voulez changer des chose ou autre

--- html/mention-legales.php
+++ html/mention-legales.php
@@ -13,40 +13,54 @@
 <body>
     <?php include("menu.php"); ?>
 
-
     <section class="text-mention-légales">
-    <h2> Editeur </H2> 
 
-    Association EFREI
-    RCS Créteil : 398 898 338
-    Siège social : 30-32 avenue de la république 94800 Villejuif
-    Standard :  01 88 28 90 00
-    E-mail : service.communication@efrei.fr
-    Directeur général : Frédéric Meunier
-    Directrice de la communication : Caroline Coignard
-    Rédactrices : Sarah Macé, Morgane Réhault, Dorothée De Clercq
-    Hébergeur : OVH – OVHCLOUD – N°SIRET : 424 761 419 00045 – 2 RUE KELLERMANN 59100 ROUBAIX- Tél : 09 72 10 10 07
+        <h2> Editeur </H2>
 
-    <h2> Droits d’auteur </H2> 
+        <p>
+            Association EFREI<br>
+            Siège social : 30-32 avenue de la république 94800 Villejuif<br>
+            Standard : 07 66 58 14 12<br>
+            E-mail : service.communication@efrei.fr<br>
+            Directeur général : Frédéric Meunier<br>
+            Directrice de la communication : Caroline Coignard<br>
+            Rédactrices : Sarah Macé, Morgane Réhault, Dorothée De Clercq<br>
+            Hébergeur : https://github.com/NathanChhay-Trinh/Projet_Web (si cela ne fonctionne pas , merci de demander a
+            Nathan CHHAY - - TRINH )<br>
+        </p>
 
-    dans le cadre du projet d'étude scolaire pour les étudiants de B1 en ingénieurie informatique a efrei promotion 2028 , <br>
-    tout cela étant pour but éducatif et dans la progrestion de projet a un but proffesionel.
 
-    <h2> Exactitude de l’information </h2>
+        <h2> Droits d’auteur </H2>
 
-    Nous remercions tout utilisateur qui constaterait des inexactitudes, des informations erronées ou incomplètes d’en informer le responsable de la publication : Dorothée De Clercq
-    ( merci Dorothée pour ce site Efrei qui nous aide a nous inspirée )
+        <p>
 
-    <h2>Crédits photos</h2>
+            dans le cadre du projet d'étude scolaire pour les étudiants de B1 en ingénieurie informatique a efrei
+            promotion 2028 , <br>
+            tout cela étant pour but éducatif et dans la progrestion de projet a un but proffesionel.
+        </p>
 
-    Agence Polyvalence<br>
-    Eric Delamarre<br>
-    Shutterstock<br>
-    Cochise Ory<br>
-    EPS, Efrei Picture Studio<br>
-    Efrei<br>
-    Fotolia<br>
-    
+        <h2> Exactitude de l’information </h2>
+
+        <p>
+
+            Nous remercions tout utilisateur qui constaterait des inexactitudes, des informations erronées ou
+            incomplètes d’en informer le responsable de la publication : Dorothée De Clercq
+            ( merci Dorothée pour ce site Efrei qui nous aide a nous inspirée )
+        </p>
+
+        <h2>Crédits photos</h2>
+
+        <p>
+
+            Agence Polyvalence<br>
+            Eric Delamarre<br>
+            Shutterstock<br>
+            Cochise Ory<br>
+            EPS, Efrei Picture Studio<br>
+            Efrei<br>
+            Fotolia<br>
+        </p>
+
     </section>
 
 
