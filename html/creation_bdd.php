5d47eec9458239aff142ec771dfd1d052a83b930
Nathan Chhay-Trinh <NathanC.T2788@gmail.com>
2026-03-31 19:32:10 +0200

    Ajout des sessions dans sql

--- html/creation_bdd.php
+++ html/creation_bdd.php
@@ -6,14 +6,12 @@ $password = 'root';
 $port = '3307';
 $dbname = "register";
 
-// 1. Connexion SANS base
 $conn = mysqli_connect($servername, $username, $password, null, $port);
 
 if (!$conn) {
     die('Erreur de connexion : ' . mysqli_connect_error());
 }
 
-// 2. Création de la base si elle n'existe pas
 $sql_db = "CREATE DATABASE IF NOT EXISTS $dbname";
 if (mysqli_query($conn, $sql_db)) {
     echo "Base de données prête !<br>";
@@ -21,7 +19,6 @@ if (mysqli_query($conn, $sql_db)) {
     die("Erreur création base : " . mysqli_error($conn));
 }
 
-// 3. Reconnexion AVEC la base
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
 if (!$conn) {
@@ -30,7 +27,6 @@ if (!$conn) {
 
 echo "Connexion réussie<br>";
 
-// 4. Création de la table
 $sql = "CREATE TABLE IF NOT EXISTS users(
     Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Identifiant VARCHAR(30) NOT NULL,
@@ -38,6 +34,19 @@ $sql = "CREATE TABLE IF NOT EXISTS users(
     Mail VARCHAR(50) NOT NULL,
     Sta_tus VARCHAR(50) NOT NULL,
     UNIQUE(Mail)
+)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Table bien créée !";
+} else {
+    echo "Erreur création table : " . mysqli_error($conn);
+}
+$sql = "CREATE TABLE IF NOT EXISTS agenda(
+    Id INT NOT NULL,
+    User_Id VARCHAR(50) NOT NULL,
+    Identifiant VARCHAR(50) NOT NULL,
+    Mail VARCHAR(50) NOT NULL
+    Date_Session VARCHAR(50) NOT NULL
 )";
 
 if (mysqli_query($conn, $sql)) {
