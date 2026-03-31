9fb47622d65632beb38a4b3f21dde9115166f44c
Maxime <masterssauvage588@gmail.com>
2026-03-31 13:21:38 +0200

    add changement acceuil et actualité

--- html/Acceuil.php
+++ html/Acceuil.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/Interface_de_base.css">
+    <link rel="stylesheet" href="../css/Acceuil.css">
     <title>Accueil</title>
 
 </head>
@@ -13,11 +13,217 @@
 <body>
 
     <?php include("menu.php"); ?>
-    <?php include("Footer.php"); ?>
+
+    <picture class="hero">
+        <div class="hero-contenu">
+        <h1>Rejoignez l'Efrei</h1>
+        <p>Une école d'ingénieur tournée vers le numérique</p>
+    </div>
+</picture>
+
+    <section class="histoire-efrei">
+        <h2>Histoire de l'Efrei</h2>
+
+        <div class ="bandeau">
+            
+        <div class="bandeau-contenu">
+
+        <div class="texte-descriptif">
+        <p>
+             Fondée en 1936 sous le nom d’École Française de Radioélectricité, EFREI Paris formait à l’origine des spécialistes des télécommunications et de l’électronique, des domaines alors en plein essor. Au fil des décennies, l’école a su évoluer avec les avancées technologiques en intégrant progressivement l’informatique, ce qui lui a permis d’obtenir la reconnaissance de la Commission des Titres d’Ingénieur et de s’imposer comme une école d’ingénieurs de référence. À partir des années 2000, elle opère un tournant vers le numérique en développant des formations en systèmes informatiques, réseaux et technologies innovantes, tout en s’ouvrant à l’international. Aujourd’hui, EFREI Paris est une grande école spécialisée dans le numérique, proposant des cursus variés en ingénierie, data, intelligence artificielle et cybersécurité, avec un fort lien avec les entreprises et l’innovation.
+        </p>
+        </div>
+        <div class="image">
+            <img src="..\img\Image_Efrei.png" alt="Efrei">
+        </div>
+</div>
+</div>
+</section>
+
+<section class="chiffre-cle">
+
+    <h2>L'Efrei en quelque chiffres</h2>
+
+    <div class="bandeau-chiffres">
+
+    <div class="stats-contenu">
+
+    <div class="stats-cle">
+    <p class="chiffre">
+        1ère
+    </p>
+    <p class="texte">
+        école privée au statut EESPIG
+    </p>
+    </div>
+       <div class="stats-cle">
+    <p class="chiffre">
+        2000
+    </p>
+    <p class="texte">
+        entreprises partenaires
+    </p>
+    </div>
+
+    <div class="stats-cle">
+    <p class="chiffre">
+        16 000
+    </p>
+    <p class="texte">
+        alumni
+    </p>
+    </div>
+    <div class="stats-cle">
+    <p class="chiffre">
+        98%
+    </p>
+    <p class="texte">
+        taux d'emplois
+        </p>
+    </div>
+    </div>
+    </div>
+</section>
+
+<section class="actualites">
+    <div class="header-actus">
+        <h2>Actualités</h2>
+
+        <div class="bouton-contenu">
+            <a href="Actualite.php" class="bouton-actu">Voir toutes les actualités →</a>
+        </div>
+</div>
+    <div class="actus-contenu">
+
+        <div class="actu">
+            <div class="image-actu">
+            <img src="..\img\IMG_4822-1024x768.jpg" alt="">
+            </div>
+            <p class="date-actu">12 mars 2026</p>
+            <h3 class="titre-actu">Engineering Projects : l’innovation</h3>
+        </div>
+
+        <div class="actu">
+            <div class="image-actu">
+            <img src="..\img\beasternchen-bees-8893198_1920-1024x796.jpg" alt="">
+            </div>
+            <p class="date-actu">9 mars 2026</p>
+            <h3 class="titre-actu">L’IA au service de l’apiculture</h3>
+        </div>
+
+        <div class="actu">
+            <div class="image-actu">
+            <img src="..\img\Efrei-X-Stem4All-44-1024x683.jpg" alt="">
+            </div>
+            <p class="date-actu">18 février 2026</p>
+            <h3 class="titre-actu">Parité dans le numérique</h3>
+        </div>
+        </div>
+</section>
+
+<section class="temoignage">
+
+    <h2>Temoignage de nos clients</h2>
+
+    <div class="carousel">
+
+    <div class="carousel-track">
+        
+    <div class="card">
+        <div class="gauche">
+            <img src="https://randomuser.me/api/portraits/men/44.jpg" class="photo">
+            <p class="nom">Maxime Sauvage</p>
+            <p class="niveau">1ère année en Bachelor Ingénierie et Numérique</p>
+        </div>
+
+    <div class="droite">
+            <p class="texte-temoignage">
+                "Grâce à l'Efrei, j'ai progressé rapidement ."
+            </p>
+    </div>
+</div>
 
 
+    <div class="card">
+        <div class="gauche">
+        <img src="https://randomuser.me/api/portraits/men/32.jpg" class="photo">
+        <p class="nom">Nathan Chhay - - Trinh</p>
+        <p class="niveau">1ère année en Cycle ingénieur</p>
+    </div>
 
+    <div class="droite">
+        <p class="texte-temoignage">
+            "Les projets sont très concrets et permettent vraiment de progresser rapidement ."
+        </p>
+    </div>
+</div>
 
+    <div class="card">
+        <div class="gauche">
+        <img src="https://randomuser.me/api/portraits/men/68.jpg" class="photo">
+        <p class="nom">Kévin Kocoglu</p>
+        <p class="niveau">2ème année en Bachelor Ingénierie et Cybersécurité</p>
+    </div>
+
+    <div class="droite">
+        <p class="texte-temoignage">
+            "Une école tournée vers l'avenir avec des formations en phase avec les besoins des entreprises."
+        </p>
+    </div>
+</div>
+
+    <div class="card">
+        <div class="gauche">
+        <img src="https://randomuser.me/api/portraits/men/75.jpg" class="photo">
+        <p class="nom">Alexandre Dos Santos</p>
+        <p class="niveau">1ère année en Bachelor Ethical Hacking</p>
+    </div>
+
+    <div class="droite">
+        <p class="texte-temoignage">
+            "L'ambiance est excellente et l'accompagnement des étudiants est vraiment un plus."
+        </p>
+    </div>
+    </div>
+</div>
+    
+    <div class="btn-container">
+    <button class="btn prev">←</button>
+    <button class="btn next">→</button>
+</div>
+
+</section>
+
+<script>
+const track = document.querySelector('.carousel-track');
+const cards = document.querySelectorAll('.card');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let index = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+next.addEventListener('click', () => {
+    index++;
+    if (index >= cards.length) {
+        index = 0; // boucle
+    }
+    updateCarousel();
+});
+
+prev.addEventListener('click', () => {
+    index--;
+    if (index < 0) {
+        index = cards.length - 1; // boucle inverse
+    }
+    updateCarousel();
+});
+</script>
+
+  <?php include("Footer.php"); ?>
 
 </body>
 
--- html/Actualite.php
+++ html/Actualite.php
@@ -4,8 +4,9 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../css/Interface_de_base.css">
-    <title>Accueil</title>
+    <link rel="stylesheet" href="../css/Actualite.css">
+    <link rel="stylesheet" href="../css/Acceuil.css">
+    <title>Actualite</title>
 
 </head>
 
@@ -13,6 +14,87 @@
 
 
     <?php include("menu.php"); ?>
+
+   <section class="page-actualites">
+
+    <h2>Actualités du moment</h2>
+
+    <div class="actus-container">
+
+        <div class="actu-card">
+            <img src="../img/IMG_4822-1024x768.jpg" alt="">
+            <p class="date-actu">12 mars 2026</p>
+            <h3 class="titre-actu">Engineering Projects : l'innovation</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/beasternchen-bees-8893198_1920-1024x796.jpg" alt="">
+            <p class="date-actu">9 mars 2026</p>
+            <h3 class="titre-actu">L'IA au service de l'apiculture</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/Efrei-X-Stem4All-44-1024x683.jpg" alt="">
+            <p class="date-actu">18 février 2026</p>
+            <h3 class="titre-actu">Parité dans le numérique</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/efrei_for_good-32-1-1024x747.jpg" alt="">
+            <p class="date-actu">11 février 2026</p>
+            <h3 class="titre-actu">Classement des école d'ingénieurs 2026 : Efrei sur le podium</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/EFG-15-1024x631.jpg" alt="">
+            <p class="date-actu">10 février 2026</p>
+            <h3 class="titre-actu">À Bordeaux, l'école d'ingénieurs Efrei se mobilise pour une Tech responsable</h3>
+            </div>
+
+        <div class="actu-card">
+            <img src="../img/AP-Stalk-PPT-1024x576.png" alt="">
+            <p class="date-actu">6 février 2026</p>
+            <h3 class="titre-actu">Stalk saison 3 à l'Efrei</h3>
+            </div>
+
+        <div class="actu-card">
+            <img src="../img/premierepierre_bordeaux-1024x640.jpg" alt="">
+            <p class="date-actu">4 février 2026</p>
+            <h3 class="titre-actu">Un chai bordelais devient un campus d'ingénieurs informatique</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/Label-carre-_Best-School-Experience_2026_SA-1024x887.png" alt="">
+            <p class="date-actu">29 janvier 2026</p>
+            <h3 class="titre-actu">L'Efrei obtient le label Best School Experience 2026</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/SIMS2-1024x683.png" alt="">
+            <p class="date-actu">21 janvier 2026</p>
+            <h3 class="titre-actu">Semestre scientifique : vers un double diplôme manager ingénieur</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/groupeetudiantsefrei-1024x681.jpg" alt="">
+            <p class="date-actu">19 janvier 2026 · Parcoursup</p>
+            <h3 class="titre-actu">Parcoursup : nos conseils pour réussir son dossier</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/Classement-Efrei-662-x-400-px.png" alt="">
+            <p class="date-actu">13 janvier 2026 · Classements</p>
+            <h3 class="titre-actu">Classements écoles d'ingénieurs 2026 : l'Efrei dans le top 10 national</h3>
+        </div>
+
+        <div class="actu-card">
+            <img src="../img/DSC06282-1024x683.jpg" alt="">
+            <p class="date-actu">8 janvier 2026 · Vie de l'école</p>
+            <h3 class="titre-actu">L'Efrei inaugure son nouveau site au coeur de Villejuif</h3>
+        </div>
+
+    
+    </section>
     <?php include("Footer.php"); ?>
 
 
